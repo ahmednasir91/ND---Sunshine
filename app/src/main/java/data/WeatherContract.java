3f0f6d71280e50825ba30869af0ae5ed674c2853
Ahmed Nasir <ahmednasir91@gmail.com>
2016-05-07 15:35:37 +0500

    Added Location Contract.

--- app/src/main/java/data/WeatherContract.java
+++ app/src/main/java/data/WeatherContract.java
@@ -8,5 +8,12 @@ public class WeatherContract {
         static final String COLUMN_DATE = "date";
         static final String COLUMN_MIN_TEMP = "min_temperature";
         static final String COLUMN_MAX_TEMP = "max_temperature";
+        static final String COLUMN_LOCATION_ID = "location_id";
+    }
+
+    public class LocationEntry implements BaseEntry {
+        static final String COLUMN_LATITUDE = "latitude";
+        static final String COLUMN_LONGITUDE = "longitude";
+        static final String COLUMN_CITY_NAME = "city_name";
     }
 }
